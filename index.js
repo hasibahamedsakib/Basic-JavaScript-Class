2bc946360f842ffae346f2a6dcc11b6e360575bf
Hasib Ahamed Sakib <hasibahamedsakib1@gmail.com>
2023-12-05 21:50:45 +0600

    added class one

---
+++ index.js
@@ -0,0 +1,86 @@
+// console.log();
+// document.write();
+
+// var variable = "first variable";
+// console.log(variable);
+
+// a,_$11
+// var x = 10;
+// const x = 10;
+// var X = 20;
+//  const x = 20;
+
+// var return = "Dream IT";
+
+// console.log(x);
+
+// var x = {
+//   name: "Hasan",
+//   age: 22,
+// };
+// console.log(x.name, x.age);
+// var array1 = "10,20, 30"
+
+// var array = [
+//   10,
+//   20,
+//   30,
+//   "jillu",
+//   false,
+//   undefined,
+//   {
+//     name: "Hasan",
+//     age: 22,
+//   },
+// ];
+
+// console.log(array[3], array[6]);
+
+// var firstName = "Mr Rahim";
+// var lastName = "Islam";
+// function concat(x, y) {
+//   console.log(x + " " + y);
+// }
+// concat(firstName, lastName);
+
+// console.log(firstName.concat(lastName));
+
+// var fulName = firstName + " " + lastName;
+// var fulName = firstName.concat(lastName)
+// var fulName = `${firstName} ${lastName} `; // backtick
+
+// + , - , *, /, %,
+
+// function operation() {
+//   var x = 20;
+//   var y = "50";
+//   var sum = x % y;
+//   console.log(sum);
+// }
+
+// operation();
+
+// console.log(fulName);
+// var num = 10;
+
+// num = num + 10; // num += 10;
+
+// num =+ 10;
+// num += 10;
+// var num2 = 10;
+
+// var num = 20;
+// // num = num * 10;
+// num *= 10;
+// console.log(num);
+
+// var x = 2.1;
+// var y = 1.5;
+// var sum = x + y;
+// console.log(parseInt(sum));
+// console.log(Math.round(sum));
+
+// function myFunction() {
+//   console.log("my function");
+// };
+// myFunction()
