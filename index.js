5ee77e4d08a1ec446b39f60e6bc358dfa560391a
Hasib Ahamed Sakib <hasibahamedsakib1@gmail.com>
2023-12-05 21:52:53 +0600

    added class two

---
+++ index.js
@@ -0,0 +1,97 @@
+// What is let and const. And what is the difference of let vs const.
+// var x;
+// console.log(x);
+
+// var x = 10;
+
+// var x;
+// x = 10
+// var x;
+// x = 20;
+// {
+//     let x;
+//     x= 10;
+// //   x = 50;
+// //   console.log("sec- x", x);
+// }
+// console.log(window);
+
+// {
+//     const x = 10;
+// //     // const x ;
+// //     // x =  10
+// }
+
+// x = 30;
+// console.log("first", x);
+
+// x = 10;
+// const x;
+// var x;
+// x = null;
+// console.log(x);
+
+// var x = 50;
+
+// Well known string method
+// let country = ["Bangldesh", "India", "pakistan"];
+// console.log(country.length);
+// let country = ["Bangladesh", "India", "pakistan"];
+// console.log(country.indexOf("India"));
+// console.log(sliceValue);
+// String toUpperCase()
+// String toLowerCase()
+// String concat()
+// let name = "mr rahim";
+// let sliceValue = name.toUpperCase().toLowerCase();
+// console.log(sliceValue);
+
+// console.log(string.replaceAll("my", "our"));
+// let text1 = "      Hello World!      ";
+// let text2 = text1.trim();
+// console.log(text2);
+
+// let string = "I love my country";
+// let spiltedValue = string.split(" ");
+// console.log(spiltedValue);
+// let text = "HELLO WORLD";
+// let char = text.charAt(4);
+
+// console.log(char);
+
+// Deep drive to array and array method.
+// let numbers = [10, 20, 30, 40, 50];
+// let newValue = numbers.push(100);
+// let newValue1 = numbers.splice(3, 0, 100);
+// let newValue1 = numbers.shift();
+// console.log(newValue);
+// console.log(numbers);
+
+// let country = ["Bangladesh", "India", "pakistan"];
+// delete country[1];
+// console.log(country.length);
+// console.log(numbers.join("00 "));
+
+// let friends = ["Rahim", "Karim", "Salam", "Borkot"];
+
+// country.push(friends);
+
+// console.log(country.flat());
+// const myArr = [
+//   [1, 2],
+//   [3, 4],
+//   [5, 6],
+// ];
+// console.log(myArr.flat());
+
+// let age = 17;
+// let hasHouse = true;
+// let hasCar = true;
+
+// if (age > 18 || hasHouse == true) {
+//   console.log("Shortlisted");
+// } else if (hasCar == true) {
+//   console.log("Amar ekta gari ase");
+// } else {
+//   console.log("amar kicui nai");
+// }
